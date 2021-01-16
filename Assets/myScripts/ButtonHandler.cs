6b98b50ecb3ef22975fa2394688fbcee90a5c27b
Charlotte Imenkamp <charlotte.imenkamp@igd-r.fraunhofer.de>
2021-01-16 13:22:15 +0100

    New Szenes and GameManager
    Mit "n" werden nun die Szenen gewechselt. Dabei muss beim Level "Start" gestartet werden, sonst funktioniert das nicht

---
+++ Assets/myScripts/ButtonHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonHandler : MonoBehaviour
+{
+	public Button yourButton;
+
+	void Start()
+	{
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		GameManager.Instance.LoadSzene();
+	}
+
+	// workaround without VR
+	void Update()
+	{
+		if (Input.GetKey("n"))
+		{
+			GameManager.Instance.LoadSzene();
+		}
+	}
+}
---
+++ Assets/myScripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Instance { get; set; }
+
+	public int m_CurrentSzeneIndex; 
+
+	// Awake Singelton
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+			DontDestroyOnLoad(this.gameObject);
+		}
+		else
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		m_CurrentSzeneIndex = 0; 
+    }
+
+	//StartButton clicked
+	public void LoadSzene()
+	{
+		if (m_CurrentSzeneIndex < 3)
+		{
+			//set new map active
+			SceneManager.LoadScene(m_CurrentSzeneIndex);
+		}
+        else
+        {
+			print("GameManager: No more Scenes available"); 
+        }
+		//go on
+		m_CurrentSzeneIndex++;
+	}
+}

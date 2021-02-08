0184b278badc52ddb19de3bdaeb03a3930561df2
Charlotte Imenkamp <charlotte.imenkamp@igd-r.fraunhofer.de>
2021-02-08 11:15:32 +0100

    Vibrations Anwendung zugefügt und erste Versuche

---
+++ Assets/LinkVibrationHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using LinkVibrationController; 
+
+public class LinkVibrationHandler : LinkVibrationManager
+{
+    private LinkVibrationManager controller;
+
+    void Start()
+    {
+        // controller = GameObject.Find("VibrationManager").GetComponent()
+
+
+    }
+
+    void Update()
+    {
+        controller.VibrateShort(1); 
+    }
+}
---
+++ Assets/myScripts/LinkVibrationManager.cs
@@ -0,0 +1,16 @@
+﻿using LinkVibrationController;
+using UnityEngine;
+
+public class LinkVibrationManager : LinkVibration
+{
+    
+    public void VibrateShort(int index)
+    {
+        Vibrate(index, 500);
+    }
+
+    public void VibrateLong(int index)
+    {
+        Vibrate(index, 1000);
+    }
+}
